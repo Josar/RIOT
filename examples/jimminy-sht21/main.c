a9b30f613f93ad4ba30215bc61668e582109406c
steffen <steffen.robertz@online.de>
2017-08-01 10:39:37 +0200

    switched vdd to 3V

--- examples/jimminy-sht21/main.c
+++ examples/jimminy-sht21/main.c
@@ -32,15 +32,16 @@ int main(void)
 	LED_PORT &= ~(RED|GREEN|BLUE);
     puts("Jimminy SHT21 test");
     printf("This will test the Temperature and Humidity sensor\n");
-    stepdown_init(2600);
+    stepdown_init(3000);
     sht21_init(I2C_0);
     _delay_ms(10);
+    sht21_write_settings(I2C_0, RES_RH12_T14);
     printf("Temp: %0.2f \n", sht21_measure(I2C_0, CRC_ENABLED, TEMP));
    printf("Humidity: %0.2f \n", sht21_measure(I2C_0, CRC_ENABLED, HUMIDITY));
    _delay_ms(1000);
    printf("Read_setting: %x \n", sht21_read_settings(I2C_0));
-   sht21_write_settings(I2C_0, RES_RH11_T11| EN_CHIP_HEATER);
-   printf("Read_setting: %x \n", sht21_read_settings(I2C_0));
+  // sht21_write_settings(I2C_0, RES_RH11_T11| EN_CHIP_HEATER);
+   //printf("Read_setting: %x \n", sht21_read_settings(I2C_0));
 while(1){
 	printf("Temp: %0.2f \n", sht21_measure(I2C_0, CRC_ENABLED, TEMP));
 	printf("Humidity: %0.2f \n", sht21_measure(I2C_0, CRC_ENABLED, HUMIDITY));
