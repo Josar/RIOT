f2c7171963e6ebde149b61d9d28ee6d3d4d45328
Josarn <josuaarndt@live.de>
2018-03-28 02:01:56 +0200

    drivers/at86rf2xx: atmega256rfr2 support

--- drivers/at86rf2xx/at86rf2xx.c
+++ drivers/at86rf2xx/at86rf2xx.c
@@ -33,10 +33,114 @@
 #include "at86rf2xx_registers.h"
 #include "at86rf2xx_internal.h"
 #include "at86rf2xx_netdev.h"
-
 #define ENABLE_DEBUG (0)
 #include "debug.h"
 
+#include "board.h"
+#define ENABLE_HEX_DUMP_TX  (0)
+#if ENABLE_HEX_DUMP_TX
+#include "od.h"
+#endif
+
+#ifdef MODULE_AT86RFR2
+
+#include "avr/interrupt.h"
+
+/* Device Pointer for Interrupt callback */
+static netdev_t *static_dev = 0;
+/* Counting Retries */
+int8_t retries = 0;
+
+/**
+ * \brief ISR for transceiver's receive end interrupt
+ *
+ *  Is triggered when valid data is received. FCS check passed.
+ *  Save IRQ status and inform upper layer of data reception.
+ */
+ISR(TRX24_RX_END_vect, ISR_BLOCK){
+    __enter_isr();
+#if defined(RXTX_LEDS)
+    LED0_TOGGLE;
+#endif
+    DEBUG("TRX24_RX_END\n");
+    ((at86rf2xx_t *)static_dev)->irq_status |= AT86RF2XX_IRQ_STATUS_MASK__RX_END;
+    /* Upper layer could also be called after TRX24_TX_END instead */
+    static_dev->event_callback(static_dev, NETDEV_EVENT_ISR);
+#if defined(RXTX_LEDS)
+    LED0_TOGGLE;
+#endif
+    __exit_isr();
+}
+
+/**
+ * \brief  Transceiver Frame Address Match, indicates incoming frame
+ *
+ *  Is triggered when Frame with valid Address is received.
+ *  Can be sused to wake up MCU from sleep, etc.
+ */
+ISR(TRX24_XAH_AMI_vect, ISR_BLOCK){
+    __enter_isr();
+    DEBUG("TRX24_XAH_AMI\n");
+    ((at86rf2xx_t *)static_dev)->irq_status |= AT86RF2XX_IRQ_STATUS_MASK__AMI;
+    __exit_isr();
+}
+
+/**
+ * \brief ISR for transceiver's transmit end interrupt
+ *
+ *  Is triggered when data or when acknowledge frames where send.
+ */
+ISR(TRX24_TX_END_vect, ISR_BLOCK){
+    at86rf2xx_t *dev = (at86rf2xx_t *) static_dev;
+
+    __enter_isr();
+    dev->irq_status |= AT86RF2XX_IRQ_STATUS_MASK__TX_END;
+
+    /* Only radios with the XAH_CTRL_2 register support frame retry reporting
+     * but with TX_START and TX_END retries can also be count */
+    ((at86rf2xx_t *)static_dev)->tx_retries = retries;
+    retries = -1;
+    uint8_t state = *AT86RF2XX_REG__TRX_STATE & AT86RF2XX_TRX_STATE_MASK__TRAC;
+
+    DEBUG("TRX24_TX_END\n");
+    /* only inform upper layer when a transmission was done,
+     * not for sending acknowledge frames if we received data */
+    if (state != AT86RF2XX_STATE_RX_AACK_ON ) {
+        /* set device back in receiving state */
+        /* TODO
+         * I seems that the upper layer don't set the receiver back to receive.
+         * I consider this a preliminary solution
+         * until i know where and how it should be done.
+         * */
+        at86rf2xx_set_state(dev, AT86RF2XX_STATE_RX_AACK_ON);
+        /* Call Upper layer to for statistics*/
+        static_dev->event_callback(static_dev, NETDEV_EVENT_ISR);
+    }
+    __exit_isr();
+}
+
+/**
+ * \brief ISR for transceiver's TX_START interrupt
+ *
+ * Indicates the frame start of a transmitted acknowledge frame in procedure RX_AACK.
+ * In procedure TX_ARET the TRX24_TX_START interrupt is issued separately for every
+ * frame transmission and frame retransmission.
+ */
+ISR(TRX24_TX_START_vect){
+    __enter_isr();
+#if defined(RXTX_LEDS)
+    LED0_TOGGLE;
+#endif
+    retries++;
+    DEBUG("TRX24_TX_START\n");
+    ((at86rf2xx_t *)static_dev)->irq_status1 |= AT86RF2XX_IRQ_STATUS_MASK1__TX_START;
+    static_dev->event_callback(static_dev, NETDEV_EVENT_ISR);
+#if defined(RXTX_LEDS)
+    LED0_TOGGLE;
+#endif
+    __exit_isr();
+}
+#endif
 
 void at86rf2xx_setup(at86rf2xx_t *dev, const at86rf2xx_params_t *params)
 {
@@ -49,11 +153,22 @@ void at86rf2xx_setup(at86rf2xx_t *dev, const at86rf2xx_params_t *params)
     /* radio state is P_ON when first powered-on */
     dev->state = AT86RF2XX_STATE_P_ON;
     dev->pending_tx = 0;
+
+#ifdef MODULE_AT86RFR2
+    /* Store device pointer for interrupts */
+    static_dev = (netdev_t *)dev;
+
+    /* set all interrupts off */
+    at86rf2xx_reg_write(dev, AT86RF2XX_REG__IRQ_MASK, 0x00);
+    at86rf2xx_reg_write(dev, AT86RF2XX_REG__IRQ_MASK1, 0x00);
+#endif
 }
 
 void at86rf2xx_reset(at86rf2xx_t *dev)
 {
     eui64_t addr_long;
+
+    DEBUG("at86rf2xx_reset(): start.\n");
 
     at86rf2xx_hardware_reset(dev);
 
@@ -106,6 +221,42 @@ void at86rf2xx_reset(at86rf2xx_t *dev)
     at86rf2xx_set_page(dev, AT86RF2XX_DEFAULT_PAGE);
 #endif
 
+#ifdef MODULE_AT86RFR2
+    /* Set idle state so device is always listening */
+    dev->idle_state = AT86RF2XX_STATE_RX_AACK_ON;
+
+    /* TODO enable necessary interrupts, maybe other interrupts could be useful */
+
+    /* enable interrupts IRQ_MASK*/
+    at86rf2xx_reg_write(dev, AT86RF2XX_REG__IRQ_MASK,
+//           AT86RF2XX_IRQ_STATUS_MASK__AWAKE             /* not */
+                        AT86RF2XX_IRQ_STATUS_MASK__TX_END /* std */
+//          | AT86RF2XX_IRQ_STATUS_MASK__AMI          /* maybe used for ED/RSSI readout*/
+//          | AT86RF2XX_IRQ_STATUS_MASK__CCA_ED_DONE  /* not */
+                        | AT86RF2XX_IRQ_STATUS_MASK__RX_END /* std */
+//          | AT86RF2XX_IRQ_STATUS_MASK__RX_START     /* do not use in Extended Operating Mode */
+//          | AT86RF2XX_IRQ_STATUS_MASK__PLL_UNLOCK   /* not */
+//          | AT86RF2XX_IRQ_STATUS_MASK__PLL_LOCK     /* not */
+                        );
+
+    /* enable interrupts IRQ_MASK1*/
+    at86rf2xx_reg_write(dev, AT86RF2XX_REG__IRQ_MASK1,
+                        AT86RF2XX_IRQ_STATUS_MASK1__TX_START /* additional for retry counter*/
+//        | AT86RF2XX_IRQ_STATUS_MASK1__MAF_0_AMI
+//        | AT86RF2XX_IRQ_STATUS_MASK1__MAF_1_AMI
+//        | AT86RF2XX_IRQ_STATUS_MASK1__MAF_2_AMI
+//        | AT86RF2XX_IRQ_STATUS_MASK1__MAF_3_AMI
+                        );
+
+    /* clear interrupt flags by writing corresponding bit */
+    at86rf2xx_reg_write( dev, AT86RF2XX_REG__IRQ_STATUS,  0xff );
+    at86rf2xx_reg_write( dev, AT86RF2XX_REG__IRQ_STATUS1, 0xff );
+    /* clear frame buffer protection */
+    *AT86RF2XX_REG__TRX_CTRL_2 &= ~(1 << RX_SAFE_MODE);
+
+    /* set PLL on */
+    at86rf2xx_set_state(dev, AT86RF2XX_STATE_PLL_ON);
+#else
     /* don't populate masked interrupt flags to IRQ_STATUS register */
     uint8_t tmp = at86rf2xx_reg_read(dev, AT86RF2XX_REG__TRX_CTRL_1);
     tmp &= ~(AT86RF2XX_TRX_CTRL_1_MASK__IRQ_MASK_MODE);
@@ -124,6 +275,7 @@ void at86rf2xx_reset(at86rf2xx_t *dev)
     /* enable interrupts */
     at86rf2xx_reg_write(dev, AT86RF2XX_REG__IRQ_MASK,
                         AT86RF2XX_IRQ_STATUS_MASK__TRX_END);
+#endif
 
     /* go into RX state */
     at86rf2xx_set_state(dev, AT86RF2XX_STATE_RX_AACK_ON);
@@ -170,6 +322,14 @@ void at86rf2xx_tx_exec(const at86rf2xx_t *dev)
 
     /* write frame length field in FIFO */
     at86rf2xx_sram_write(dev, 0, &(dev->tx_frame_len), 1);
+#if ENABLE_HEX_DUMP_TX
+    uint8_t len = dev->tx_frame_len;
+    uint8_t data[len];
+    memcpy( data, (void *)(AT86RF2XX_REG__TRXFBST), len);
+    puts("SENDING:");
+    od_hex_dump(data, len, OD_WIDTH_DEFAULT);
+#endif
+
     /* trigger sending of pre-loaded frame */
     at86rf2xx_reg_write(dev, AT86RF2XX_REG__TRX_STATE,
                         AT86RF2XX_TRX_STATE__TX_START);
@@ -185,6 +345,7 @@ bool at86rf2xx_cca(at86rf2xx_t *dev)
     uint8_t old_state = at86rf2xx_set_state(dev, AT86RF2XX_STATE_TRX_OFF);
     /* Disable RX path */
     uint8_t rx_syn = at86rf2xx_reg_read(dev, AT86RF2XX_REG__RX_SYN);
+
     reg = rx_syn | AT86RF2XX_RX_SYN__RX_PDT_DIS;
     at86rf2xx_reg_write(dev, AT86RF2XX_REG__RX_SYN, reg);
     /* Manually triggered CCA is only possible in RX_ON (basic operating mode) */
